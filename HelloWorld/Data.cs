d72c20292eb21de218afed92b6242b48992426bc
Sergey <adamas34@gmail.com>
2021-12-13 02:50:18 +0300

    addClassData

---
+++ HelloWorld/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+using Tekla.Structures.Geometry3d;
+
+namespace HelloWorld
+{
+    class Data
+    {
+        int coordX = 0;
+        int coordY = 0;
+        int coordZ = 0;
+        public void DataVar(int x, int y, int z)
+        {
+            this.coordX = x;
+            this.coordY = y;
+            this.coordZ = z;
+        }
+    }
+}
