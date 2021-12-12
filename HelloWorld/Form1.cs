7e7accd15c1bc5455724b00bfee1b1c6267025a9
Sergey <adamas34@gmail.com>
2021-12-12 22:55:31 +0300

    letter E done

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -82,61 +82,80 @@ namespace HelloWorld
             model.CommitChanges();
 
             //letter E
-            var firstPointE1 = new Point(0, 8000, 0);
+            var firstPointE1 = new Point(6000, 8000, 0);
 
-            var secondPointE1 = new Point(0, 8000, 5000);
+            var secondPointE1 = new Point(6000, 8000, 5000);
             var profileE1 = new Profile { ProfileString = "UC310*97" };
             var materialE1 = new Material { MaterialString = "300PLUS" };
             var classE1 = "2";
 
-            var beamH1 = new Beam
+            var beamE1 = new Beam
             {
-                StartPoint = firstPointH1,
-                EndPoint = secondPointH1,
-                Profile = profileH1,
-                Material = materialH1,
-                Class = classH1
+                StartPoint = firstPointE1,
+                EndPoint = secondPointE1,
+                Profile = profileE1,
+                Material = materialE1,
+                Class = classE1
             };
 
-            beamH1.Position.Depth = Position.DepthEnum.MIDDLE;
+            beamE1.Position.Depth = Position.DepthEnum.MIDDLE;
 
-            //второй столбец
-            var firstPointH2 = new Point(3000, 8000, 0);
-            var secondPointH2 = new Point(3000, 8000, 5000);
-            var profileH2 = new Profile { ProfileString = "UC310*97" };
-            var materialH2 = new Material { MaterialString = "300PLUS" };
-            var classH2 = "2";
+            //hor bottom beam 
+            var firstPointE2 = new Point(6000, 8000, 0);
+            var secondPointE2 = new Point(9000, 8000, 0);
+            var profileE2 = new Profile { ProfileString = "UB360*51" };
+            var materialE2 = new Material { MaterialString = "300PLUS" };
+            var classE2 = "1";
 
-            var beamH2 = new Beam
+            var beamE2 = new Beam
             {
-                StartPoint = firstPointH2,
-                EndPoint = secondPointH2,
-                Profile = profileH2,
-                Material = materialH2,
-                Class = classH2
+                StartPoint = firstPointE2,
+                EndPoint = secondPointE2,
+                Profile = profileE2,
+                Material = materialE2,
+                Class = classE2
             };
 
-            beamH2.Position.Depth = Position.DepthEnum.MIDDLE;
+            beamE2.Position.Depth = Position.DepthEnum.FRONT;
 
-            //hor beam
-            var firstPointH3 = new Point(0, 8000, 2500);
-            var secondPointH3 = new Point(3000, 8000, 2500);
-            var profileH3 = new Profile { ProfileString = "UB360*51" };
-            var materialH3 = new Material { MaterialString = "300PLUS" };
-            var classH3 = "1";
+            //hor middle beam
+            var firstPointE3 = new Point(0, 8000, 2500);
+            var secondPointE3 = new Point(3000, 8000, 2500);
+            var profileE3 = new Profile { ProfileString = "UB360*51" };
+            var materialE3 = new Material { MaterialString = "300PLUS" };
+            var classE3 = "1";
 
-            var beamH3 = new Beam
+            var beamE3 = new Beam
             {
-                StartPoint = firstPointH3,
-                EndPoint = secondPointH3,
-                Profile = profileH3,
-                Material = materialH3,
-                Class = classH3
+                StartPoint = firstPointE3,
+                EndPoint = secondPointE3,
+                Profile = profileE3,
+                Material = materialE3,
+                Class = classE3
             };
 
-            beamH1.Insert();
-            beamH2.Insert();
-            beamH3.Insert();
+            //hor top beam
+            var firstPointE4 = new Point(6000, 8000, 5000);
+            var secondPointE4 = new Point(9000, 8000, 5000);
+            var profileE4 = new Profile { ProfileString = "UB360*51" };
+            var materialE4 = new Material { MaterialString = "300PLUS" };
+            var classE4 = "1";
+
+            var beamE4 = new Beam
+            {
+                StartPoint = firstPointE4,
+                EndPoint = secondPointE4,
+                Profile = profileE4,
+                Material = materialE4,
+                Class = classE4
+            };
+
+            beamE4.Position.Depth = Position.DepthEnum.BEHIND;
+
+            beamE1.Insert();
+            beamE2.Insert();
+            beamE3.Insert();
+            beamE4.Insert();
 
             model.CommitChanges();
         }
