0d76387a90bbf54010557873a889a9b0152c0de8
Sergey <adamas34@gmail.com>
2021-12-16 00:54:03 +0300

    letterW

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -38,7 +38,6 @@ namespace HelloWorld
             LetterD letterD = new LetterD();
             letterD.LetterDCreate();
 
-
             // letter L
             LetterL letterL1 = new LetterL();
             letterL1.LetterFirstLCreate();
@@ -58,65 +57,8 @@ namespace HelloWorld
 
 
             //letter W
-
-            var firstPointW1 = new Point(x * 4 + 305, 0, 0);
-            var secondPointW1 = new Point(x * 4 + 305, 0, 1000);
-            var profileW1 = new Profile { ProfileString = "UC310*97" };
-            var materialW1 = new Material { MaterialString = "300PLUS" };
-            var classW1 = "2";
-
-            var beamW1 = new Beam
-            {
-                StartPoint = firstPointW1,
-                EndPoint = secondPointW1,
-                Profile = profileW1,
-                Material = materialW1,
-                Class = classW1
-            };
-
-            beamW1.Position.Depth = Position.DepthEnum.MIDDLE;
-
-            //второй столбец
-            var firstPointW2 = new Point(x * 4, 0, 1000);
-            var secondPointW2 = new Point(x * 4, 0, z);
-            var profileW2 = new Profile { ProfileString = "UC310*97" };
-            var materialW2 = new Material { MaterialString = "300PLUS" };
-            var classW2 = "2";
-
-            var beamW2 = new Beam
-            {
-                StartPoint = firstPointW2,
-                EndPoint = secondPointW2,
-                Profile = profileW2,
-                Material = materialW2,
-                Class = classW2
-            };
-
-            beamW2.Position.Depth = Position.DepthEnum.MIDDLE;
-
-            beamW1.Insert();
-            beamW2.Insert();
-
-            //slope beam
-            var firstPointW3 = new Point(x * 4 + 305, 0, 1000);
-            var secondPointW3 = new Point(x * 4 + x / 2, 0, z / 2);
-            var profileW3 = new Profile { ProfileString = "UB360*51" };
-            var materialW3 = new Material { MaterialString = "300PLUS" };
-            var classW3 = "1";
-
-            var beamW3 = new Beam
-            {
-                StartPoint = firstPointW3,
-                EndPoint = secondPointW3,
-                Profile = profileW3,
-                Material = materialW3,
-                Class = classW3
-            };
-
-            beamW3.Position.Depth = Position.DepthEnum.BEHIND;
-
-            beamW3.Insert();
-            
+            LetterW letterW = new LetterW();
+            letterW.LetterWCreate();
 
             model.CommitChanges();
         }
---
+++ HelloWorld/LetterW.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+using Tekla.Structures.Geometry3d;
+
+namespace HelloWorld
+{
+    class LetterW
+    {
+        public void LetterWCreate()
+        {
+            Data data = new Data();
+            data.DataVar(3000, 4000, 5000);
+            var x = data.x;
+            var y = data.y;
+            var z = data.z;
+
+            //first column
+            VerticalColumn beamW1 = new VerticalColumn();
+
+            var firstPointW1 = new Point(x * 4, 0, 1000);
+            var secondPointW1 = new Point(x * 4, 0, z);
+
+            beamW1.Column(firstPointW1, secondPointW1);
+
+            //second column
+            VerticalColumn beamW2 = new VerticalColumn();
+
+            var firstPointW2 = new Point(x * 4 + 305, 0, 0);
+            var secondPointW2 = new Point(x * 4 + 305, 0, 1000);
+
+            beamW2.Column(firstPointW2, secondPointW2);
+
+            //third column
+            VerticalColumn beamW3 = new VerticalColumn();
+
+            var firstPointW3 = new Point(x * 5, 0, 1000);
+            var secondPointW3 = new Point(x * 5, 0, z);
+
+            beamW3.Column(firstPointW3, secondPointW3);
+
+            //fourth column
+            VerticalColumn beamW4 = new VerticalColumn();
+
+            var firstPointW4 = new Point(x * 5 - 305, 0, 0);
+            var secondPointW4 = new Point(x * 5 - 305, 0, 1000);
+
+            beamW4.Column(firstPointW4, secondPointW4);
+
+            //first beam
+            HorizontalBeam beamW5 = new HorizontalBeam();
+
+            var firstPointW5 = new Point(x * 4 + 305, 0, 1000);
+            var secondPointW5 = new Point(x * 4 + x / 2, 0, z / 2);
+
+            beamW5.HorBeam(firstPointW5, secondPointW5, Position.DepthEnum.BEHIND);
+
+            //second beam
+            HorizontalBeam beamW6 = new HorizontalBeam();
+
+            var firstPointW6 = new Point(x * 5 - 305, 0, 1000);
+            var secondPointW6 = new Point(x * 5 - x / 2, 0, z / 2);
+
+            beamW6.HorBeam(firstPointW6, secondPointW6, Position.DepthEnum.BEHIND);
+        }
+    }
+}
